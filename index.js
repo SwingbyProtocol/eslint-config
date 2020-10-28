bf9cbf95f130d49f9e5fd106626922c6bff0e5fb
Rubén <ruben@swingby.network>
2020-10-28 09:42:52 +0100

    feat: Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/babel",
+    "prettier/react",
+  ],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "import/named": ["off"],
+    "import/order": ["error", { "newlines-between": "always" }],
+  },
+};
